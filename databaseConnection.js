118c57c388c83c0375b85cbfc806c93cf35df9b5
BCITKevin <kso51@my.bcit.ca>
2024-04-17 17:05:37 -0700

    starter code

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,7 +2,15 @@ const mysql = require('mysql2/promise');
 
 const is_render = process.env.IS_RENDER || false;
 
-const dbConfigQoddi = {
+// const dbConfigQoddi = {
+// 	host: "sql.freedb.tech",
+// 	user: "freedb_2350_main..",
+// 	password: "svW5VCbNE?gY*q3",
+// 	database: "freedb_comp2350-week5-A01296033",
+// 	multipleStatements: false,
+// 	namedPlaceholders: true
+// };
+const dbConfigLocal = {
 	host: "sql.freedb.tech",
 	user: "freedb_2350_main..",
 	password: "svW5VCbNE?gY*q3",
@@ -11,14 +19,14 @@ const dbConfigQoddi = {
 	namedPlaceholders: true
 };
 
-const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "kevin02gusgh02",
-	database: "database1",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
+// const dbConfigLocal = {
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "kevin02gusgh02",
+// 	database: "database1",
+// 	multipleStatements: false,
+// 	namedPlaceholders: true
+// };
 
 if (is_render) {
 	var database = mysql.createPool(dbConfigQoddi);
